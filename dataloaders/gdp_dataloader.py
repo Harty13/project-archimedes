d98a897f7b7287b86a32b926cd12764913af8763
Erik <erikli.schnell@gmail.com>
2025-08-05 13:43:11 +0200

    Calculating education quantity and quality and sum.

--- dataloaders/gdp_dataloader.py
+++ dataloaders/gdp_dataloader.py
@@ -1,18 +1,12 @@
-import pandas as pd
+from .dataloader import DataLoader
 
-class GDPDataLoader:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.countries = None
+class GDPDataLoader(DataLoader):
+    def __init__(self, file_path, countries=None):
+        super().__init__(file_path)
+        self.countries = countries
         self.data = None
 
-
-    def _load_from_csv(self):
-        """Load GDP data from a CSV file."""
-        raw_pd_data = pd.read_csv(self.file_path)
-        return raw_pd_data
-
-    def _process_raw_data(self, raw_pd_data):
+    def _process_data(self, raw_pd_data):
         """Process the raw GDP data. Returns a DataFrame with GDP values. (country, year, gdp)"""
         processed_data = raw_pd_data.copy()
         
@@ -30,17 +24,3 @@ class GDPDataLoader:
             processed_data = processed_data[processed_data['country'].isin(self.countries)]
         
         return processed_data
-
-
-    def load_data(self):
-        """Load GDP data from the specified file path. Returns a DataFrame with GDP values for the specified countries."""
-        try:
-            raw_pd_data = self._load_from_csv()
-            data = self._process_raw_data(raw_pd_data)
-            return data
-
-        except FileNotFoundError:
-            print(f"File not found: {self.file_path}")
-        except Exception as e:
-            print(f"An error occurred while loading data: {e}")
-
